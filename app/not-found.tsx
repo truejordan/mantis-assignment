734f9ed4a09889627f860016ff43c239a7c48db3
truejordan <jordan.atlanta.henry@gmail.com>
2024-11-07 00:14:33 +0000

    not-found remove arrow function

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,13 @@
 import React from "react";
+import Link from "next/link";
 
-const Notfound = () => {
+export default function NotFound() {
   return (
-    <div className="flex w-full h-screen justify-center items-center">
-      <h1 className="text-3xl font-medium text-center text-white/20">
-        404
-      </h1>
+    <div className="flex flex-col w-full h-screen justify-center items-center">
+      <h1 className="text-3xl font-medium text-center text-white/20">404</h1>
+      <Link href={"/"}>
+        <p className="text-center text-white/10 hover:text-green-mantis/60 mt-3">Home</p>
+      </Link>
     </div>
   );
-};
-
-export default Notfound;
+}

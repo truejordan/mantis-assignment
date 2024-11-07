03e7ba9470523558c1459686225f17b62190122d
truejordan <jordan.atlanta.henry@gmail.com>
2024-11-07 01:10:28 +0000

    made async use server

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,8 @@
+'use server'
 import React from "react";
 import Link from "next/link";
 
-export default function NotFound() {
+export default async function NotFound() {
   return (
     <div className="flex flex-col w-full h-screen justify-center items-center">
       <h1 className="text-3xl font-medium text-center text-white/20">404</h1>

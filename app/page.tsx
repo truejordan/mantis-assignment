46eaa28cfaa5661d0a0d94ac5910fefad1e6108f
truejordan <jordan.atlanta.henry@gmail.com>
2024-11-08 15:45:38 +0000

    clean up

--- app/page.tsx
+++ app/page.tsx
@@ -22,9 +22,6 @@ const Home = async () => {
     process.env.NODE_ENV === "production"
       ? `https://${process.env.VERCEL_URL || 'mantis-assignment-43af.vercel.app'}`
       : "http://localhost:3000";
-  console.log("Base URL:", baseUrl);
-  console.log("vercel url", process.env.VERCEL_URL)
-  console.log("VERCEL_ENV:", process.env.NODE_ENV);
   
   const res = await fetch(`${baseUrl}/api/block`, { cache: "no-store" });
   
@@ -33,18 +30,6 @@ const Home = async () => {
     console.log("Response:", res.status, errorText );
     throw new Error("Failed to fetch data");
   }
-
-  // try {
-  //   console.log("Starting fetch to /api/block");
-  //   const res = await fetch(`${baseUrl}/api/block`, { cache: "no-store" });
-
-  //   console.log("Fetch status:", res.status);
-
-  //   if (!res.ok) {
-  //     const errorText = await res.text();
-  //     console.error("Fetch failed:", res.status, errorText);
-  //     throw new Error(`Fetch failed with status: ${res.status}`);
-  //   }
 
   const data: BlockData[] = await res.json();
 
@@ -56,9 +41,5 @@ const Home = async () => {
       </div>
     </div>
   );
-// } catch (error) {
-//   console.error("Error during fetch:", error);
-//   throw error;
-// }
 };
 export default Home;

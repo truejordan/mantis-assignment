1694e32e0155591bffb0446f2b5535e07058068f
truejordan <jordan.atlanta.henry@gmail.com>
2024-11-08 10:33:53 +0000

    move error json respons in to if statement

--- app/page.tsx
+++ app/page.tsx
@@ -25,12 +25,14 @@ const Home = async () => {
         }`
       : "http://localhost:3000";
   console.log("Base URL:", baseUrl);
+  console.log("vercel url", process.env.VERCEL_URL)
   console.log("VERCEL_ENV:", process.env.NODE_ENV);
   
   const res = await fetch(`${baseUrl}/api/block`, { cache: "no-store" });
-  console.log("Response:", res.status, await res.text());
-
+  
   if (!res.ok) {
+    const errorText = await res.text()
+    console.log("Response:", res.status, errorText );
     throw new Error("Failed to fetch data");
   }
 
